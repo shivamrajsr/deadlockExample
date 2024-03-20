0a6a32e7e2b38e9c67baafb10bc935f5ac197d67
shivamrajsr <shivam703368@gmail.com>
2024-03-20 12:53:11 +0530

    explanation added with sout

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,15 @@ public class Main {
         new Thread(()->{
             synchronized (lockOne){
                 try {
+                    System.out.println("Thread 1 acquired lockOne");
                     Thread.sleep(1000);
                 }
                 catch (InterruptedException e){
                     throw new RuntimeException();
                 }
+                System.out.println("thread 1 waiting for lockTwo");
                 synchronized (lockTwo){
-                    System.out.println("both locks acquired");
+                    System.out.println(" Thread 1 acquired lockTwo!!");
                 }
             }
 
@@ -25,17 +27,24 @@ public class Main {
         new Thread(()->{
             synchronized (lockTwo){
                 try {
+                    System.out.println("Thread 2 acquired lockTwo");
                     Thread.sleep(1000);
                 }
                 catch (InterruptedException e){
                     throw new RuntimeException();
                 }
+                System.out.println("thread 2 waiting for lockOne");
+                System.out.println("deadlock is acheived as both threads are waiting for objects\n" +
+                        "which are acquired by both parties\n" +
+                        "i.e Thread1 has acquired Lock One and Thread 2 has acquired LockTwo.");
                 synchronized (lockOne){
-                    System.out.println("both locks acquired");
+                    System.out.println("Thread2 acquired lockOne!!");
                 }
             }
 
         }).start();
+
+
 
 //        Thread 1: Acquires lock1 and then tries to acquire lock2.
 //        Thread 2: Acquires lock2 and then tries to acquire lock1.
